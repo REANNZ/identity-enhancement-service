5ded03d2239f6637ba78dcbdb282eec71567829a
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2016-06-23 14:51:12 +1000

    Update ApplicationController per Gumboot

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,16 @@ class ApplicationController < ActionController::Base
   private_constant :Unauthorized
   rescue_from Unauthorized, with: :unauthorized
 
+  BadRequest = Class.new(StandardError)
+  private_constant :BadRequest
+  rescue_from BadRequest, with: :bad_request
+
   protect_from_forgery with: :exception
   before_action :ensure_authenticated
   after_action :ensure_access_checked
 
   def subject
-    subject = session[:subject_id] && Subject.find_by_id(session[:subject_id])
+    subject = session[:subject_id] && Subject.find_by(id: session[:subject_id])
     return nil unless subject.try(:functioning?)
     @subject = subject
   end
@@ -20,7 +24,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def ensure_authenticated
-    return redirect_to('/auth/login') unless session[:subject_id]
+    return force_authentication unless session[:subject_id]
 
     @subject = Subject.find_by(id: session[:subject_id])
     raise(Unauthorized, 'Subject invalid') unless @subject
@@ -60,5 +64,11 @@ class ApplicationController < ActionController::Base
     flash.now[:error] =
       [message, object.errors.full_messages.join("\n")].join("\n\n")
     render(view)
+  end
+
+  def force_authentication
+    session[:return_url] = request.url if request.get?
+
+    redirect_to('/auth/login')
   end
 end
